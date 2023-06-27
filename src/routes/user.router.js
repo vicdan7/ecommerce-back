73cfdd4d5db6115adbb9dce38068c070e138d77a
vicdan7 <ibarravictor334@gmail.com>
2023-06-27 12:18:09 -0500

    works

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,14 +1,18 @@
-const { getAll, create, remove, update } = require('../controllers/user.controllers');
+const { getAll, create, remove, update, login } = require('../controllers/user.controllers');
 const express = require('express');
+const verifyJWT = require("../utils/verifyJWT")
 
 const routerUser = express.Router();
 
 routerUser.route('/')
-    .get(getAll)
-    .post(create);
+    .get(verifyJWT,getAll)
+    .post(create)
+
+routerUser.route('/login')
+    .post(login)
 
 routerUser.route('/:id')
-    .delete(remove)
-    .put(update);
+    .delete(verifyJWT,remove)
+    .put(verifyJWT,update);
 
 module.exports = routerUser;
