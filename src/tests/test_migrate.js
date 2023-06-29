096aa9f5df2a67933a71110ff65f91ab830d698b
vicdan7 <ibarravictor334@gmail.com>
2023-06-28 19:08:49 -0500

    first test

--- src/tests/test_migrate.js
+++ src/tests/test_migrate.js
@@ -1,15 +1,16 @@
-const sequelize = require('../utils/connection');
-const user = require('./createData/user');
+const sequelize = require("../utils/connection");
+const user = require("./createData/user");
+require("../models/Category")
 
-const main = async() => {
-    try{
-        await sequelize.sync({ force: true });
-        await user()
-        console.log("Vic,me ejecuté");
-        process.exit();
-    } catch(error){
-        console.log(error);
-    }
-}
+const main = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    await user();
+    console.log("Vic, me ejecuté");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 main();
